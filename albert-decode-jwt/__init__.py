d606ba4635d01b69f3ad72607d98743c03ef7952
Jan Eisenmenger <jan.eisenmenger@tngtech.com>
2021-12-10 16:37:19 +0100

    allow users to pick their own fields if available

--- albert-decode-jwt/__init__.py
+++ albert-decode-jwt/__init__.py
@@ -11,6 +11,13 @@ __triggers__ = "jwt"
 __authors__ = "Jan Eisenmenger"
 
 def get_item_for_field(field, decoded_token):
+    if (field not in decoded_token):
+        return Item(id = 'decode-jwt-{}'.format(field),
+        text = field, 
+        subtext = str('field "{}" does not exist on the jwt'.format(field)),
+        actions = [])  
+
+    
     return Item(
         id = 'decode-jwt-{}'.format(field),
         text = field, 
@@ -48,8 +55,14 @@ def handleQuery(query):
             subtext = str(e),
             actions = [] )]
         
+    fields_of_interest = ['sub', 'scope', 'iss']
+    
+    if len(query_tokens) > 1:
+        fields_of_interest.extend([query_tokens[1]])
+        
+    info(fields_of_interest)
      
-    items = list(map(lambda f: get_item_for_field(f, decoded_token), ['sub', 'scope', 'iss']))
+    items = list(map(lambda f: get_item_for_field(f, decoded_token), fields_of_interest))
     items.extend([Item(
         id = 'decode-jwt-obj',
         text = 'Decoded token', 
