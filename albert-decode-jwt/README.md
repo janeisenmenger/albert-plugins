9d15a23e6f1b2a16634be840d2c56ae67034b209
Jan Eisenmenger <jan.eisenmenger@tngtech.com>
2021-12-10 16:29:18 +0100

    Add new plugin to decode a jwt

---
+++ albert-decode-jwt/README.md
@@ -0,0 +1,2 @@
+# albert-decode-jwt
+An Albert extension to decode a JWT and get the fields that you want.
---
+++ albert-decode-jwt/__init__.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+"""An Albert extension to decode a JWT and get the fields that you want."""
+
+from albert import info, Item, ClipAction
+import jwt
+
+__title__ = "Decode JWT"
+__version__ = "0.0.1"
+__triggers__ = "jwt"
+__authors__ = "Jan Eisenmenger"
+
+def get_item_for_field(field, decoded_token):
+    return Item(
+        id = 'decode-jwt-{}'.format(field),
+        text = field, 
+        subtext = str(decoded_token[field]),
+        actions = [
+            ClipAction(
+                text=field,
+                clipboardText=str(decoded_token[field])
+                )]    
+    )
+
+def handleQuery(query):
+    if not query.isTriggered:
+        return
+        
+    query_tokens = list(filter(lambda t: t != '' , query.string.split(' '))) 
+    if (len(query_tokens) == 0):
+        return [Item(
+            id = 'decode-jwt-hint',
+            text = 'jwt <token>', 
+            subtext = 'paste your token to decode it',
+            actions = []
+        )]
+        
+    decoded_token = {}
+        
+    try :
+        decoded_token = jwt.decode(
+            query_tokens[0],
+            options={"verify_signature": False, 'verify_aud': False})
+    except Exception as e: 
+        return [Item(
+              id = 'decode-jwt-issue',
+            text = 'Could not decode the token', 
+            subtext = str(e),
+            actions = [] )]
+        
+     
+    items = list(map(lambda f: get_item_for_field(f, decoded_token), ['sub', 'scope', 'iss']))
+    items.extend([Item(
+        id = 'decode-jwt-obj',
+        text = 'Decoded token', 
+        subtext = str(decoded_token),
+        actions = [
+            ClipAction(
+                text='Decoded token',
+                clipboardText=str(decoded_token)
+                )]    
+    )])        
+    
+    return items   
+    
